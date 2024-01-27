63e16ef2dc5b5131db0da11cacf20cf165665886
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-01-27 17:12:20 -0500

    created the express app and added port 3000 for server

--- server.js
+++ server.js
@@ -2,3 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+
+// Express application and port
+const app = express();
+const PORT = process.env.PORT || 3000;
