f830409f7263d285012720f09fa09d81d29f2399
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-01-27 17:15:53 -0500

    set up middleware to handle JSON parsing

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ const fs = require('fs');
 // Express application and port
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// JSON parsing with middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
